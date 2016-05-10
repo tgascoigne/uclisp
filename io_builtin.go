99c9db538894fb8d47d7e31aadccbb845a8ea051
Tom Gascoigne <tom@gascoigne.me>
2016-05-11 00:53:25 +0100

    implement load-file

--- io_builtin.go
+++ io_builtin.go
@@ -2,10 +2,12 @@ package uclisp
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 )
 
 func init() {
+	Builtin.Define("load-file", Procedure(loadfileForm))
 	Builtin.Define("printf", Procedure(printfForm))
 }
 
@@ -29,4 +31,27 @@ func printfForm(env Env, args []Elem) Elem {
 	out := fmt.Sprintf(string(format), argsIface...)
 	log.Println(out)
 	return String(out)
+}
+
+func loadfileForm(parentEnv Env, args []Elem) Elem {
+	if len(args) != 1 {
+		Raise(ErrArgCount, len(args))
+	}
+
+	path, err := AssertString(args[0].Eval(parentEnv))
+	if err != nil {
+		Raise(err)
+	}
+
+	//todo: better logging
+	fmt.Printf("load-file: %v\n", path)
+
+	data, err := ioutil.ReadFile(string(path))
+	if err != nil {
+		Raise(err)
+	}
+
+	env := NewBasicEnv(parentEnv)
+	prog := Parse(string(path), string(data))
+	return prog.Eval(env)
 }
