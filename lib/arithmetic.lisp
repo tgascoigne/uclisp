c854a693bfbec4d5502e0f0fa33dd5ed49b2961e
Tom Gascoigne <tom@gascoigne.me>
2016-05-16 21:32:15 +0100

    stdlib: implement inc, dec

---
+++ lib/arithmetic.lisp
@@ -0,0 +1,35 @@
+(progn
+
+  (defmacro inc (sym &optional count)
+    (unless count (setq count 1))
+    `(progn
+       (setq ,sym (+ ,sym ,count))
+       ,sym))
+
+  (defmacro dec (sym &optional count)
+    (unless count (setq count 1))
+    `(progn
+       (setq ,sym (- ,sym ,count))
+       ,sym))
+
+  (deftests
+
+    (test-expect 2
+                 (let ((x 1))
+                   (inc x)))
+
+    (test-expect 3
+                 (let ((x 1))
+                   (inc x 2)))
+
+    (test-expect 9
+                 (let ((x 10))
+                   (dec x)))
+
+    (test-expect 8
+                 (let ((x 10))
+                   (dec x 2)))
+
+    )
+
+  )
