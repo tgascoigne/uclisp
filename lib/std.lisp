c3462ede7ff40e7b39f226b9bdbdfa415de82f05
Tom Gascoigne <tom@gascoigne.me>
2016-05-16 21:32:49 +0100

    stdlib: implement add-to-list, reverse, list-contains-p

--- lib/std.lisp
+++ lib/std.lisp
@@ -2,4 +2,7 @@
   (load-file "lib/decl.lisp")
   (load-file "lib/test/framework.lisp")
   (load-file "lib/cond.lisp")
+  (load-file "lib/arithmetic.lisp")
+  (load-file "lib/iter.lisp")
+  (load-file "lib/listutil.lisp")
   )
