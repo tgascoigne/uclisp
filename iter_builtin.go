6fb00e486b904d709aaec2480655903ceec5160d
Tom Gascoigne <tom@gascoigne.me>
2016-05-16 21:31:45 +0100

    implement while

---
+++ iter_builtin.go
@@ -0,0 +1,19 @@
+package uclisp
+
+func init() {
+	Builtin.Define("while", Procedure(whileForm))
+}
+
+func whileForm(env Env, args []Elem) Elem {
+	if len(args) < 1 {
+		Raise(ErrArgCount, len(args))
+	}
+
+	test := args[0]
+	body := append(List{Symbol("progn")}, args[1:]...)
+	for !IsNil(test.Eval(env)) {
+		body.Eval(env)
+	}
+
+	return Nil
+}
